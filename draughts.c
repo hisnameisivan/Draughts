a55c252c61e935a9b05d4524f6c18ea9b248a40c
Walda rivers Addam <waddam@mi-w6.21-school.ru>
2019-05-09 20:48:01 +0300

    add arguments

--- draughts.c
+++ draughts.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-unsigned long long	func(int x, int y)
+/*
+** Программа для подсчета способов пройти шашке в дамки из положения (x, y),
+** на доске размерностью dim. Нумерация ячеек по горизонтали и вертикали
+** начинается с '0', размерность с '1'. Программа, запущенная без аргументов,
+** вычисляет количество способов для поля 8x8, где шашка находится на первой,
+** строке, четвертом столбце. Аналогично запуску
+** ./a.out 3 0 8
+*/
+
+unsigned long long	func(int x, int y, int dim)
 {
-	if (x < 0 || x > 7)
+	if (x < 0 || x > dim)
 		return (0);
-	if (y == 7)
+	if (y == dim)
 		return (1);
-	return (func(x + 1, y + 1) + func(x - 1, y + 1));
+	return (func(x + 1, y + 1, dim) + func(x - 1, y + 1, dim));
 }
 
-int					main(void)
+int					main(int argc, char **argv)
 {
 	int					x;
 	int					y;
+	int					dim;
 	unsigned long long	result;
 
-	x = 3;
-	y = 0;
+	if (argc == 1)
+	{
+		x = 3;
+		y = 0;
+		dim = 7;
+	}
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		dim = atoi(argv[3]) - 1;
+	}
 	result = 0;
-	result = func(x, y);
+	result = func(x, y, dim);
 	printf("%llu\n", result);
 	return (0);
 }
